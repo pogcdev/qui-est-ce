7bd12ec03bce1a888ab9ff53e1a8b1c8e9b28dc2
pogcdev <92727129+pogcdev@users.noreply.github.com>
2022-01-30 16:50:19 +0100

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ const Card = ({ personnage }) => {
             <img src={personnage.fichier} alt={personnage.prenom} />
           </div>
           <div className="flip-box-back">
-            <img src="personnages/croix.png" alt="croix" id="croix" width="100" height="100" />
+            <img src="https://zupimages.net/viewer.php?id=22/04/gp1x.png" alt="croix" id="croix" width="100" height="100" />
           </div>
         </div>
       </div>
