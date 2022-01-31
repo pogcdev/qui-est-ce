21a595b43deabc4980e123029b72ef6633f7b2da
pogcdev <92727129+pogcdev@users.noreply.github.com>
2022-01-31 14:52:18 +0100

    Update Characters.js

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -7,7 +7,7 @@ const Characters = () => {
 
   useEffect(() => {
     axios
-      .get("https://api.jsonbin.io/b/61f6b461fb3ece3ad7ce7091")
+      .get("./fichier_json/data.json")
       .then((res) => setData(res.data[0].possibilites));
   }, []);
 
